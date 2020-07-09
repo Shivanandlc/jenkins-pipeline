849080c77e0567be31fc974949857567e9e4f515
shiva <shivanand7210@gmail.com>
2020-07-10 00:05:08 +0530

    controller updated

--- src/main/java/com/demo/jenkins/controller.java
+++ src/main/java/com/demo/jenkins/controller.java
@@ -1,9 +1,11 @@
 package com.demo.jenkins;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/rest/hello")
 public class controller {
     @GetMapping("/")
     public String sayHello() {
